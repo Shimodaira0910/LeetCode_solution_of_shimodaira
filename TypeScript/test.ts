a7f01a90241844fe2b3268774abb1d16aed8cae4
下平大輝 <shimodairadaiki@shimodairadaiakiranoMacBook-Air.local>
2023-09-19 19:31:10 +0900

    twosum

--- TypeScript/test.ts
+++ TypeScript/test.ts
@@ -1,4 +1,11 @@
-let aaa: string = 'test';
-
-console.log(aaa);
-
+for(let i = 1; 100 >= i; i++){
+    if(i % 15 === 0){
+        console.log('FizzBuzz');
+    }else if(i % 5 === 0){
+        console.log('Buzz');
+    }else if(i % 3 === 0){
+        console.log('Fizz');
+    }else{
+        console.log(i)
+    }
+}
